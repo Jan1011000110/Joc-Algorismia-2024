05eb0c7747a9ac2336c0d500a7c5207fe0f1aedb
Jan <janmatasc@gmail.com>
2024-11-24 21:21:40 +0100

    fixed spell cast and add some more functions

--- game/AIHarry.cc
+++ game/AIHarry.cc
@@ -21,89 +21,59 @@ struct PLAYER_NAME : public Player {
   /**
    * Types and attributes for your player can be defined here.
    */
+
+  const int NUM_INGREDIENTS = 15;
+  const int NUM_GROUPS = 5;
+  const int NUM_INGREDIENTS_IN_GROUP = NUM_INGREDIENTS/NUM_GROUPS;
+  const int NUM_ROUNDS = 200;
   
   /**
    * Helper functions
    */
 
-  static vector<int> solve_spell (vector<int> a) {
-    int N = (int) a.size();
-
-    vector<int> pref(N + 1);
-    for (int i = 0; i < N; ++i) {
-      pref[i + 1] = pref[i] + a[i];
+  bool f(int i, int target, vector<int> &groups, vector<int> &sum, vector<int> &cnt, vector<int> &a) {
+    if (i == NUM_INGREDIENTS) {
+      return true;
     }
-    int S = pref[N];
+    for (int g = 0; g < NUM_GROUPS; ++g) {
+      if (cnt[g] < NUM_INGREDIENTS_IN_GROUP and sum[g] + a[i] <= target) {
+        groups[i] = g;
 
-    // dp[i][s1][s2] = -1 <=> not a reachable state
-    // dp[i][s1][s2] = 0 <=> reachable state and we place i-th item into first group
-    // dp[i][s1][s2] = 1 <=> reachable state and we place i-th item into second group
-    // dp[i][s1][s2] = 2 <=> reachable state and we place i-th item into third group
-    // dp[i][s1][s2] = 3 <=> reachable state and we havent placed nothigh yet
-    vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(S + 1, vector<int>(S + 1, -1)));
-    dp[0][0][0] = 3; // mark this state as a reachable one
-
-    for (int i = 0; i < N; ++i) {
-      for (int s1 = 0; s1 <= S; ++s1) {
-        for (int s2 = 0; s2 <= S; ++s2) {
-          if (dp[i][s1][s2] == -1) { // we haven't been able to reach this state
-            continue;
-          }
-          if (s1 + a[i] <= S) { // place into first group
-            dp[i + 1][s1 + a[i]][s2] = 0; 
-          }
-          if (s2 + a[i] <= S) { // place into second group
-            dp[i + 1][s1][s2 + a[i]] = 1;
-          }
-          if (pref[i] - s1 - s2 + a[i] <= S) { // place into third group
-            dp[i + 1][s1][s2] = 2;
-          }
+        cnt[g] += 1;
+        sum[g] += a[i];
+        if (f(i + 1, target, groups, sum, cnt, a)) {
+          return  true;
         }
+        cnt[g] -= 1;
+        sum[g] -= a[i];
       }
     }
+    return false;
+  }
 
-    cerr << "HERE" << endl;
-    // reconstruct the groups
-    vector<int> groups;
-    int s1 = S / 3, s2 = S / 3;
-    for (int i = N; i >= 1; --i) {
-      if (dp[i][s1][s2] == 0) {
-        s1 -= a[i - 1];
-        groups.push_back(0);
-      }
-      else if (dp[i][s1][s2] == 1) {
-        s2 -= a[i - 1];
-        groups.push_back(1);
-      }
-      else if (dp[i][s1][s2] == 2) {
-        groups.push_back(2);
-      }
-      else {
-        assert(false);
-      }
-    } 
-    reverse(groups.begin(), groups.end());
+  vector<int> solve_spell (vector<int> &a) {
+    int S = 0;
+    for (int i = 0; i < NUM_INGREDIENTS; ++i) {
+      S += a[i];
+    }
+    vector<int> groups(NUM_INGREDIENTS), sum(NUM_GROUPS), cnt(NUM_GROUPS);
+    f(0, S / NUM_GROUPS, groups, sum, cnt, a);
     return groups;
   }
 
   void cast_spell() {
     int id = ghost(me());
-    if (unit(id).resting_rounds() > 0) { // ghost can't cast spells yet
+    if (unit(id).resting_rounds() > 0 or round() > NUM_ROUNDS - 50) { // ghost is resting or last 50 rounds
       return;
     }
     vector<int> a = spell_ingredients();
     vector<int> groups = solve_spell(a);
-    // int s1 = 0, s2 = 0, s3 = 0;
-    // for (int i = 0; i < (int) a.size(); ++i) {
-    //   cerr << a[i] << " ";
-    //   if (groups[i] == 0) s1 += a[i];
-    //   else if (groups[i] == 1) s2 += a[i];
-    //   else s3 += a[i];
-    // }
-    // cerr << endl;
-    // cerr << s1 << " " << s2 << " " << s3 << endl;
-    // cerr << "TOTAL SUM = " << s1+s2+s3 << endl;
     spell(id, groups);
+  }
+
+
+  bool valid_pos(Pos p) {
+    return pos_ok(p) and cell(p).type == Corridor;
   }
 
   /**
